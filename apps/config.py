a1e47c912cf7edd50e88c5d46b37f4d86ee4eba6
Julio.sales <juliosaels509@gmail.com>
2025-08-01 15:52:24 -0400

    config email

--- apps/config.py
+++ apps/config.py
@@ -13,6 +13,16 @@ class Config(object):
     USERS_ROLES  = { 'ADMIN'  :1 , 'USER'      : 2 }
     USERS_STATUS = { 'ACTIVE' :1 , 'SUSPENDED' : 2 }
     
+    
+    # Mail Config
+    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'True').lower() in ['true', '1', 'yes']
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'False').lower() in ['true', '1', 'yes']
+    MAIL_USERNAME = os.getenv('MAIL_USER', '')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', '')
+    
+    
     # celery 
     CELERY_BROKER_URL     = "redis://localhost:6379"
     CELERY_RESULT_BACKEND = "redis://localhost:6379"
--- apps/utils/send_email.py
+++ apps/utils/send_email.py
@@ -2,3 +2,10 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+from apps.config import MAIL_SERVER, MAIL_PORT, MAIL_USE_TLS, MAIL_USER, MAIL_PASSWORD
+
+
+print('Sending email configuration loaded from environment variables.')
+print(f'MAIL_SERVER: {MAIL_SERVER}, MAIL_PORT: {MAIL_PORT}, MAIL_USE_TLS: {MAIL_USE_TLS}')
+print(f'MAIL_USER: {MAIL_USER}')
+print(f'MAL_PASSWORD: {"*" * len(MAIL_PASSWORD) if MAIL_PASSWORD else "Not set"}')
